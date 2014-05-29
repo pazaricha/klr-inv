f624cb4bae8db29bbc698d33a2b92b3ba4720b46
Paz Aricha <paz@pazaricha.com>
2014-05-29 23:12:15 +0300

    nice refactoring progress

--- spec/invoice_parser_spec.rb
+++ spec/invoice_parser_spec.rb
@@ -18,16 +18,4 @@ RSpec.describe InvoiceParser do
     end
   end
 
-  describe "#disassemble_line_to_groups_of_3_chars" do
-    let(:sample_line) do 
-      file = File.open(user_story_1_input)
-      line = file.readline.gsub("\n", "")
-      file.rewind
-      line
-    end
-    it "returns an array of groups of 3 chars" do
-      subject.disassemble_line_to_groups_of_3_chars(sample_line).should == [[" ", "_", " "], [" ", "_", " "], [" ", "_", " "], [" ", " ", " "], [" ", "_", " "], [" ", "_", " "], [" ", " ", " "], [" ", "_", " "], [" ", " ", " "]]
-    end
-  end
-
 end
